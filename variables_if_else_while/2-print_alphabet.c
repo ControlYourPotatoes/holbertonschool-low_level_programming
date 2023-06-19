90a79baa645fc6256a6d10eb81443dda2fe6766c
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-06-19 18:55:27 +0000

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main Print the alphabet in lowercase
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+	char alphabet;
+	for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
+	{
+		 putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
