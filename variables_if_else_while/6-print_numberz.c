a786a434d767b2ab5b242926a4be540798b42a88
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-06-20 14:50:50 +0000

    6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Print the alphabet in lowercase
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 48; x < 58; x++)
+	putchar(x);
+	putchar('\n');
+	return (0);
+}
