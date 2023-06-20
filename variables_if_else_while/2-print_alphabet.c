de2c0ed4d904b2e7c08c9988db821bb4464890c1
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-06-20 13:38:23 +0000

    2-print_alphabet.c

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -11,10 +11,9 @@
 int main(void)
 {
 	char alphabet;
-	for (alphabet = 'a'; c <= 'z'; alphabet++)
-	{
-		 putchar(alphabet);
-	}
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	putchar(alphabet);
 	putchar('\n');
 	return (0);
 }
