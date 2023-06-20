0091e87a6f800362a773727b1f9ccdcbc354b836
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-06-20 14:40:19 +0000

    5-print_numbers.c

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- * main - Print the alphabet in lowercase
+ * main - Print the alphabet in lowercase exluding q and e
  *
  * Return: Always (Success)
  */
---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Print the alphabet in lowercase
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+    int x;
+
+    for (x = 48; x < 58; x++)
+	putchar(x);
+	return (0);
+}
