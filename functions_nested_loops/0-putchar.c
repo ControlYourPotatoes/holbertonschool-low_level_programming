4510eb3a7881aeddfe26f71385dcc5137962f8e5
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-06-21 13:39:03 +0000

    0-putchar.c

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Print the alphabet in lowercase
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+	printf("_putchar")
+	return (0);
+}
