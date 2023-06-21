dec2ec85c6323c1551a7544049f208ee64d5338f
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-06-21 18:52:10 +0000

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Print putchar
+ *
+ * Return: Always (Success)
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n');
+}
