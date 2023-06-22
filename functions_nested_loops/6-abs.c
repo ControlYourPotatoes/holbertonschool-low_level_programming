0641347cacbd937a60433d25558c672f9855a1be
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-06-22 15:52:44 +0000

    6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _abs - Compute absolute value
+ * @n: variable
+ *
+ * Return: Absolute Value
+ */
+int _abs(int n)
+{
+	int absolute;
+
+	if (n < 0)
+	{
+		absolute = n * (-1);
+	}
+	return (absolute);
+}
