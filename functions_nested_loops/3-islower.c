3f00105b7f9af04e8c2d7a37c1b0b37d984ef09b
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-06-22 12:43:48 +0000

    3-islower.c

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <ctype.h>
+/**
+ * _islower - check if lowercase
+ * @c: variable
+ *
+ * Return: 1 (Lowercase) else 0 (False)
+ */
+int _islower(char c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	return (0);
+}
+
