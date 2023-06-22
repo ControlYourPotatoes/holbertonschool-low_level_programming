41c09cf5d87c4cd3f8eaea63a1eee0e16662c018
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-06-22 19:37:24 +0000

    9-times_table.c

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -15,11 +15,24 @@ void times_table(void)
 		{
 			int result = i * j;
 
-			_putchar(result / 10 + '0');
-			_putchar(result % 10 + '0');
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
+			if (j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (result >= 10)
+			{
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
+			}
+			else if (result < 10 && j != 0)
+			{
+				_putchar(' ');
+				_putchar((result % 10) + '0');
+			}
+			else
+				_putchar ((result % 10) + '0');
+
 		}
 		_putchar('\n');
 	}
