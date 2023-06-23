836541cb4d79198fc61e6bf89284fca837f31f3c
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-06-23 13:18:39 +0000

    7-print_last_digit.c

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -4,12 +4,19 @@
  * print_last_digit - Compute absolute value
  * @n: variable
  *
- * Return: ss
+ * Return: n = last digitbr
  */
 int  print_last_digit(int n)
 {
 	if (n < 0)
+	{
 		n = n * -1;
-	_putchar(n % 10 + '0');
-	return (n % 10);
+		_putchar(n % 10 + '0');
+
+	}
+	else
+	{
+		_putchar(n % 10 + '0');
+	}
+		return (n % 10);
 }
