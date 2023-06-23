0ec0a51be84dc9eea03c868b0061a56431f9318b
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-06-23 19:22:45 +0000

    0-isupper.c

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Print putchar
+ *
+ * Return: Always (Success)
+ */
+
+int _isupper(int c);
+{
+	if (c < 94 && c > 64 )
+	{
+		return (1);
+	}
+	else
+		return (0)
+}
+
+
+
