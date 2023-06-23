cad9e4e17b53f1d7e282ec563b99631ada3771d3
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-06-23 19:43:07 +0000

    2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - multiply two variables
+ * @a: variable 1
+ * @b: variable 2
+ * Return: Always (Success)
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
