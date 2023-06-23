41eac7d38430ff7d1f3250dabe475a3c376934c0
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-06-23 20:04:10 +0000

    5-more_numbers.c

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - alphabet print
+ *
+ * Return: Always (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int count = 0;
+
+	while (count <= 9)
+	{
+	for (c = '0'; c <= "14"; c++)
+		{
+		_putchar(c);
+		}
+		_putchar('\n');
+		count++;
+	}
+}
