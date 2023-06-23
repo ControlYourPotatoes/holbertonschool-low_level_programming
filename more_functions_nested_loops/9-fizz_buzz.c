88810e4830b6803bff0e1a254e9da53743007a40
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-06-23 23:06:25 +0000

    9-fizz_buzz.c

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - 1 to 100 but replacing multiple of 5 and 3
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz\n");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz\n");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz\n");
+		}
+		else
+		{
+			printf("%d\n", i);
+		}
+	}
+	return (0);
+}
+
