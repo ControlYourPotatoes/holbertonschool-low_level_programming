1bd4532f012d225dc21c65b9413bd83b2cea487a
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-06-24 01:24:50 +0000

    9-fizz_buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -2,39 +2,41 @@
 #include "main.h"
 
 /**
- * main - 1 to 100 but replacing multiple of 5 and 3
+ * main - 1 to 100 but replacing multiples of 5 and 3
  *
  * Return: Always 0
  */
-
 int main(void)
 {
-	int i;
+    int i;
+    int printSpace = 0;
 
-	for (i = 1; i <= 100; i++)
-	{
-		if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf("FizzBuzz");
-		}
-		else if (i % 3 == 0)
-		{
-			printf("Fizz");
-		}
-		else if (i % 5 == 0)
-		{
-			printf("Buzz");
-		}
-		else
-		{
-			printf("%d ", i);
-		}
-		if (i != 100)
-		{
-			printf(" ");
-		}
-	}
-	putchar('\n');
-	return (0);
+    for (i = 1; i <= 100; i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+        {
+            printf("%sFizzBuzz", printSpace ? " " : "");
+            printSpace = 1;
+        }
+        else if (i % 3 == 0)
+        {
+            printf("%sFizz", printSpace ? " " : "");
+            printSpace = 1;
+        }
+        else if (i % 5 == 0)
+        {
+            printf("%sBuzz", printSpace ? " " : "");
+            printSpace = 1;
+        }
+        else
+        {
+            printf("%s%d", printSpace ? " " : "", i);
+            printSpace = 1;
+        }
+    }
+
+    printf("\n");
+
+    return 0;
 }
 
