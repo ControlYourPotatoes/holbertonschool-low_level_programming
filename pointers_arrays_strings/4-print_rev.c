4d30adba88c8b370f172e142eb848069f4b1541f
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-06-27 17:52:40 +0000

    ./4-print_rev.c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -10,11 +10,9 @@ void print_rev(char *s)
 	int len;
 	int i;
 
-	for (len = 0; s[len] != '\0'; len++) {}
-
+	for (len = 0; s[len] != '\0'; len++)
+		{}
 	for (i = len - 1; i >= 0; i--)
-	{
-	    _putchar(s[i]);
-	}
+		_putchar(s[i]);
 	_putchar('\n');
 }
