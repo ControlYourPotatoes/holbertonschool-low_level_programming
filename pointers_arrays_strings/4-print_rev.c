d2eeaff583ad0122f0850cb0488fe5d45db06eb4
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-06-27 17:50:46 +0000

    ./4-print_rev.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - print a string in reverse
+ * @s: the string to put
+ */
+
+void print_rev(char *s)
+{
+	int len;
+	int i;
+
+	for (len = 0; s[len] != '\0'; len++) {}
+
+	for (i = len - 1; i >= 0; i--)
+	{
+	    _putchar(s[i]);
+	}
+	_putchar('\n');
+}
