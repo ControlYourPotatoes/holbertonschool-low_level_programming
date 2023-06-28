cb3af7be3021e0957edb60bcfc18c93a66b639e6
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-06-28 14:24:14 +0000

    ./6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -5,15 +5,15 @@
  * puts2 - Prints every other character
  * @str: the string to print
 */
-
 void puts2(char *str)
 {
 	int i = 0;
 
-		for (; i < _strlen(str); i += 1)
-		{
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
 			_putchar(str[i]);
-		}
+		i++;
+	}
 	_putchar('\n');
 }
-
