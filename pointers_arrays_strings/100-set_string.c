4c84dde2394b38c4ae249f5b0b0104acefc20460
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-06-30 19:27:32 +0000

    ./100-set_string.c

---
+++ pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * set_string - set the value of string to char
+ * @s: pointer of pointer
+ * @to: char to point
+ */
+
+void set_string(char **s char *to)
+{
+	*s = to
+}
