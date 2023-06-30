6559b50c83ba343085befd83305641fd73cc4d37
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-06-30 16:15:21 +0000

    ./2-strchr.c

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -4,18 +4,18 @@
 /**
  * _strchr - locates a character in the string
  * @s: the string
- * c: the character
+ * @c: the character
  * Return: pointer to the location of the character
 */
 
 char *_strchr(char *s, char c)
 {
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
+	for (; ; s++)
 	{
-		if (s[i] == c)
-			return (s + i);
+		if (*s == c)
+			return (s);
+		if (*s == 0)
+			return (NULL);
 	}
 	return (NULL);
 }
