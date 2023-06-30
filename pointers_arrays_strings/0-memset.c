c6d729b9f6afeb1645c54e17c310ccf8089b7542
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-06-30 14:56:30 +0000

    ./0-memset.c

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _memset - Fill memory with constant byte
+ * @s: pointer to memory area to fill
+ * @n: number of bytes to fill
+ * @b: constant byte to fill
+ * Return: pointer to memory area to fill
+ */
+
+void *_memset(void *s, int n, int b)
+{
+	unsigned char *p = s;
+
+	while (n--)
+		*p++ = b;
+	return (s);
+}
