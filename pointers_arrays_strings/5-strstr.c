1a5516bfd60f6cb655f3608d3f732ec14cf16ba7
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-06-30 17:34:07 +0000

    ./5-strstr.c

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strstr - find a string in a string
+ * @haystack: The source string
+ * @needle: The string you are looking for
+ * Return: Return pointer to the found string or NULL if not found
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *temp = haystack;
+
+	while (*haystack != '\0')
+	{
+		while (*needle == *haystack)
+		{
+			haystack++, needle++;
+		}
+		if (*needle == '\0')
+			return (temp);
+		haystack++;
+	}
+	return (0);
+}
