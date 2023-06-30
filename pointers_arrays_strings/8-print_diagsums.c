c7ed8b5db976e664dd4f31a16b5631086bb114cd
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-06-30 19:24:14 +0000

    ./8-print_diagsums.c

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -8,25 +8,16 @@
  * @size: size of the square matrix
 */
 
-void print_diagsums(int **a, int size)
+void print_diagsums(int *a, int size)
 {
 	int sum1 = 0;
 	int sum2 = 0;
 	int i;
-	int j;
 
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size; j++)
-		{
-			if (i == j)
-			{
-				sum1 += a[i][j];
-			}
-			if (i + j == size - 1)
-			{
-				sum2 += a[i][j];
-			}
-		}
+		sum1 += a[i * size + i];
+		sum2 += a[(i + 1) * size - (i + 1)];
 	}
+	printf("%d, %d\n", sum1, sum2);
 }
