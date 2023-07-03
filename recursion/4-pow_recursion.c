98cb4b979b90d590493b6f33292e798cbcc0e0f4
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-07-03 17:59:50 +0000

    ./4-pow_recursion.c

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _pow_recursion - X to the power of Y
+ * @x: base value
+ * @y: power of
+ * Return: the result
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+		return (1);
+	else if (y % 2 == 0)
+		return (_pow_recursion(x, y / 2) * _pow_recursion(x, y / 2));
+	else if (y < 0)
+		return (-1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
