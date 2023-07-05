b83fe2e2e03da11ce1561de478aed71e4e9a3820
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-07-05 18:50:36 +0000

    ./1-args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Print the name of the program
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	argv[0] = 0;
+	for (i = 0; i < argc; i++)
+	{}
+
+	printf("%d\n", i);
+	return (0);
+}
