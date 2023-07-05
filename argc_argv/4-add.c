375c8c7a278d2d0a162ffbf2fe559bd38899a78c
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-07-05 21:42:16 +0000

    ./4-add.c

---
+++ argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main- Sums the arguments
+ * @argc: number of arguments
+ * @argv: list of arguments
+ * Return: 0 if successful
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		int num = atoi(argv[i]);
+
+		if (num < 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+			sum += num;
+
+	}
+	printf("%d\n", sum);
+	return (0);
+}
