b5c7d7133267fc1ba71e116db30f1287333f34e8
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-07-18 19:31:55 +0000

    ./4-new_dog.c

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+/**
+ * new_dog - creates a new dog
+ * @name: new dog's name
+ * @age: new dog's age
+ * @owner: new dog's owner
+ * Return: pointer to new dog struct
+ **/
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *newDog = (dog_t *)malloc(sizeof(dog_t));
+
+	if (newDog == NULL)
+	{
+		return (NULL);
+	}
+
+	newDog->name = (char *)malloc(strlen(name) + 1);
+
+	if (newDog->name == NULL)
+	{
+		free(newDog);
+		return (NULL);
+	}
+	strcpy(newDog->name, name);
+
+	newDog->owner = (char *)malloc(strlen(owner) + 1);
+
+	if (newDog->owner == NULL)
+	{
+		free(newDog->name);
+		free(newDog);
+		return (NULL);
+	}
+	strcpy(newDog->owner, owner);
+
+	newDog->age = age;
+
+	return (newDog);
+}
