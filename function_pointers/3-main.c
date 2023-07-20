44dbc74c2df62ac8f1044adaea431f7be7eaca48
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-07-20 19:05:20 +0000

    ./3-main.c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -11,27 +11,19 @@
 
 int main(int argc, char *argv[])
 {
+	int num1, num2;
+
 	if (argc != 4)
-	{
-		printf("Error\n");
-		return (1);
-	}
+		printf("Error\n"), exit(98);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[3]);
-	char *operator = argv[2];
+	if (get_op_func(argv[2]) == NULL)
+		printf("Error\n"), exit(99);
 
-	int (*func)(int, int) = get_op_func(operator);
+	if (num2 == 0 && (*argv[2] == '%' || *argv[2] == '/'))
+		printf("Error\n"), exit(100);
 
-	if (func == NULL)
-	{
-		printf("Error\n");
-		return (2);
-	}
-
-	int result = func(num1, num2);
-
-	printf("%d\n", result);
-
+	printf("%d\n", (get_op_func(argv[2])(num1, num2)));
 	return (0);
 }
