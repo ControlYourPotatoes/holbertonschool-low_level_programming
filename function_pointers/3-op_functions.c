dd6b0d70a831256e3b78ee5e71fa836a24414851
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-07-20 18:53:08 +0000

    ./3-op_functions.c

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include "3-calc.h"
+#include <stdio.h>
+
+/**
+ * op_add - adds two values
+ * @a: the first value
+ * @b: the second value
+ * Return: Returns the result
+ */
+
+int op_add(int a, int b)
+{
+	return (a < b);
+}
+
+/**
+ * op_sub - substract two values
+ * @a: the first value
+ * @b: the second value
+ * Return: Returns the result
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - adds two values
+ * @a: the first value
+ * @b: the second value
+ * Return: Returns the result
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Divides two values
+ * @a: the first value
+ * @b: the second value
+ * Return: Returns the result
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - adds two values
+ * @a: the first value
+ * @b: the second value
+ * Return: Returns the result
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
