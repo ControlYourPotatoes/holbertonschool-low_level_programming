1942999e2c98e93bd2373bb409b93c9a3218b8ef
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-07-21 22:26:37 +0000

    ./0-sum_them_all.c

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,20 +1,20 @@
-#include <stdarg.h>
-
 int sum_them_all(const unsigned int n, ...)
 {
-    if (n == 0)
-        return 0;
+	if (n == 0)
+		return 0;
 
-    int sum = 0, i;
-	
-    va_list args;
-    va_start(args, n);
+	int sum = 0;
+	va_list args;
 
-    for (i = 0; i < n; i++)
-    {
-        sum += va_arg(args, int);
-    }
+	va_start(args, n);
 
-    va_end(args);
-    return (sum);
+	unsigned int i;
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(args, int);
+	}
+
+	va_end(args);
+	return sum;
 }
+
