703910fcee86da968e15a7b3749ead759cd37051
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-07-21 23:06:23 +0000

    ./1-print_numbers.c

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -3,13 +3,16 @@
 /**
  * print_numbers - print numbers
  * @n: amount to print
+ * @separator: the separator used in between
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
+	unsigned int i;
 	va_list args;
+
 	va_start(args, n);
 
-	for (unsigned int i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(args, int));
 		if (i < n - 1 && separator != NULL)
