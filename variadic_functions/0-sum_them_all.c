704859e962308fc5ce93020952a9f82ad4da9687
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-07-21 22:43:34 +0000

    ./0-sum_them_all.c

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -7,24 +7,20 @@
  * @n: number of values
  *
  */
+
 int sum_them_all(const unsigned int n, ...)
 {
-	if (n == 0)
-		return 0;
-
-	int sum;
-
-	sum = 0;
-	va_list args;
-
-	va_start(args, n);
-
 	unsigned int i;
+	int sum = 0;
+	va_list arg;
+
+	if (n == 0)
+		return (0);
+	va_start(arg, n);
 	for (i = 0; i < n; i++)
 	{
-		sum += va_arg(args, int);
+		sum += va_arg(arg, int);
 	}
-
-	va_end(args);
-	return sum;
+	va_end(arg);
+	return (sum);
 }
