47a83d7a8c1fbebe2d8f23d1bc2c8bd3199e0f8d
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-08-04 17:06:39 -0400

    indent

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -1,20 +1,20 @@
 #include "lists.h"
 
 /*
-* sum_dlistint - Sums all values in the list
-* Return: Result of sum
-*/
+ * sum_dlistint - Sums all values in the list
+ * Return: Result of sum
+ */
 
 int sum_dlistint(dlistint_t *head)
 {
-    int sum = 0;
-    dlistint_t *current = head;
+	int sum = 0;
+	dlistint_t *current = head;
 
-    while (current != NULL)
-    {
-        sum += current->n;
-        current = current->next;
-    }
+	while (current != NULL)
+	{
+		sum += current->n;
+		current = current->next;
+	}
 
-    return (sum);
+	return (sum);
 }
