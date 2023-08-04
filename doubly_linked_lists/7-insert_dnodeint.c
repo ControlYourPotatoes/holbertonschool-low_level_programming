f0278d703aa1406eb8d14e4093aea2bc9e93837f
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-08-04 17:29:16 -0400

    betty

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -9,40 +9,40 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    dlistint_t *new_node, *current = *h;
-    unsigned int count = 0;
+	dlistint_t *new_node, *current = *h;
+	unsigned int count = 0;
 
-    if (idx == 0)
-        return (add_dnodeint(h, n));
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 
-    /* Find the node at the specified index or the last node */
-    while (current != NULL && count < idx)
-    {
-        current = current->next;
-        count++;
-    }
+	/* Find the node at the specified index or the last node */
+	while (current != NULL && count < idx)
+	{
+		current = current->next;
+		count++;
+	}
 
-    /* If idx is out of range and not inserting at the end, return NULL */
-    if (current == NULL && count < idx)
-        return (NULL);
+	/* If idx is out of range and not inserting at the end, return NULL */
+	if (current == NULL && count < idx)
+		return (NULL);
 
-    /* If inserting at the end, use add_dnodeint_end function */
-    if (current == NULL && count == idx)
-        return (add_dnodeint_end(h, n));
+	/* If inserting at the end, use add_dnodeint_end function */
+	if (current == NULL && count == idx)
+		return (add_dnodeint_end(h, n));
 
-    /* insert at current when idx is reached */
-    new_node = malloc(sizeof(dlistint_t));
-    if (new_node == NULL)
-        return (NULL);
+	/* insert at current when idx is reached */
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    new_node->n = n;
-    new_node->prev = current->prev;
-    new_node->next = current;
+	new_node->n = n;
+	new_node->prev = current->prev;
+	new_node->next = current;
 
-    if (current->prev != NULL)
-        current->prev->next = new_node;
+	if (current->prev != NULL)
+		current->prev->next = new_node;
 
-    current->prev = new_node; /*end*/
+	current->prev = new_node; /*end*/
 
-    return (new_node);
+	return (new_node);
 }
