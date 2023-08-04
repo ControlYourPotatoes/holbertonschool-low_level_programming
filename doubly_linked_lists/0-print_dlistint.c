0f167cd9ff4584a2a509cd319533e5068e0487d0
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-08-04 13:43:03 -0400

    space fix

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,15 +1,14 @@
 #include "lists.h"
 #include <stdio.h>
 
-size_t print_dlistint(const dlistint_t *h) {
-	size_t node_count = 0;
-
-	while (h != NULL) {
-		printf("%d\n ", h->n);
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
 		h = h->next;
-		node_count++;
 	}
-
-	printf("\n");
-	return node_count;
+	return (count);
 }
