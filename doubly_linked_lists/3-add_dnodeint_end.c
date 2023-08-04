6897701287cc657a91bf67625901592fc7dfc924
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-08-04 18:14:11 +0000

    ./3-add_dnodeint_end.c

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -7,34 +7,36 @@
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node;
-    new_node = malloc(sizeof(dlistint_t));
-    if (new_node == NULL) /* if failed to allocate memory  */
-    {
-        return (NULL);
-    }
+	dlistint_t *new_node;
 
-    new_node->n = n;
-    new_node->next = NULL;
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL) /* if failed to allocate memory  */
+	{
+		return (NULL);
+	}
 
-    if (*head == NULL)
-    {
-        /* If the list is empty, make the new node the head */
-        new_node->prev = NULL;
-        *head = new_node;
-    }
-    else
-    {
-        /* Find the last node and move pointers */
-        dlistint_t *last_node = *head;
-        while (last_node->next != NULL)
-        {
-            last_node = last_node->next;
-        }
+	new_node->n = n;
+	new_node->next = NULL;
 
-        last_node->next = new_node;
-        new_node->prev = last_node;
-    }
+	if (*head == NULL)
+	{
+		/* If the list is empty, make the new node the head */
+		new_node->prev = NULL;
+		*head = new_node;
+	}
+	else
+	{
+		/* Find the last node and move pointers */
+		dlistint_t *last_node = *head;
 
-    return (new_node);
+		while (last_node->next != NULL)
+		{
+			last_node = last_node->next;
+		}
+
+		last_node->next = new_node;
+		new_node->prev = last_node;
+	}
+
+	return (new_node);
 }
