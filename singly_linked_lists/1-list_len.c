df29d9474b5def1ec7f1f738ffa00d8856f01914
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-08-04 11:44:38 -0400

    1-listlen

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ *list_len - prints all the nodes of a linked list
+ *@h: points to the link struct
+ *Return: the number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+
+    size_t count = 0;
+
+    while (h != NULL) /*Check empty*/
+    {
+        if (h->str != NULL)
+        {
+            count++;
+        }
+        h = h->next;
+    }
+    return (count);
+}
