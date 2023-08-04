4417147d4fc8eaaa2d8f8c458413ad0f01b513f7
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-08-04 17:25:00 +0000

    ./3-add_node_end.c

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -10,42 +10,36 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *newTail;
+	list_t *newTail;
 
-    if (str == NULL)
-        return (NULL);
-
-    newTail = (list_t *)malloc(sizeof(list_t));
-
-    if (newTail == NULL)
-        return (NULL);
-
-    newTail->str = strdup(str);
-    if (newTail->str == NULL)
-    {
-        free(newTail);
-        return (NULL);
-    }
-
-    newTail->len = strlen(str);
-    newTail->next = NULL; /* Make sure the newTail points to NULL (end of the list). */
-
-    if (*head == NULL)
-    {
-        /* If empty, make the newTail the head. */
-        *head = newTail;
-    }
-    else
-    {
-        /* Traverse the list to last. */
-        list_t *current = *head;
-        while (current->next != NULL)
-        {
-            current = current->next;
-        }
-
-        /* Add the newTail after the last node. */
-        current->next = newTail;
-    }
-    return (newTail); /* Return the address of the newTail node. */
+	if (str == NULL)
+		return (NULL);
+	newTail = (list_t *)malloc(sizeof(list_t));
+	if (newTail == NULL)
+		return (NULL);
+	newTail->str = strdup(str);
+	if (newTail->str == NULL)
+	{
+		free(newTail);
+		return (NULL);
+	}
+	newTail->len = strlen(str);
+	newTail->next = NULL; /* Make sure the newTail points to NULL (end of the list). */
+	if (*head == NULL)
+	{
+		/* If empty, make the newTail the head. */
+		*head = newTail;
+	}
+	else
+	{
+		/* Traverse the list to last. */
+		list_t *current = *head;
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+		/* Add the newTail after the last node. */
+		current->next = newTail;
+	}
+	return (newTail); /* Return the address of the newTail node. */
 }
