05e6b4c030d37f6bb0c657fdbc71100a70b37015
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-08-04 17:56:15 +0000

    ./2-add_dnodeint.c

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -4,24 +4,24 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node;
+	dlistint_t *new_node;
 
-    new_node = malloc(sizeof(dlistint_t));
-    
-    if (new_node == NULL) /* if empty */
-    {
-        return NULL;
-    }
+	new_node = malloc(sizeof(dlistint_t));
 
-    new_node->n = n;
-    new_node->prev = NULL;
-    new_node->next = *head;
+	if (new_node == NULL) /* if empty */
+	{
+		return (NULL);
+	}
 
-    if (*head != NULL)
-    {
-        (*head)->prev = new_node;
-    }
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *head;
 
-    *head = new_node;
-    return new_node;
+	if (*head != NULL)
+	{
+		(*head)->prev = new_node;
+	}
+
+	*head = new_node;
+	return (new_node);
 }
