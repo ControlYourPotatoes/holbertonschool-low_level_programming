cdb0d37c9da9ab7c28e75af2b4125d9192a20be3
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-08-04 13:28:56 -0400

    4-free_list.c

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - frees the memory of a list.
+ * @head: pointer to a list.
+ *
+ * Return: Nothing
+ */
+
+void free_list(list_t *head)
+{
+    list_t *current, *next;
+
+    current = head;
+
+    while (current != NULL)
+    {
+        next = current->next;
+        free(current->str);
+        free(current);
+        current = next;
+    }
+}
