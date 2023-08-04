7e201836968a83e45e276e7df3718bcc9219b09a
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-08-04 13:44:01 -0400

    betty

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,9 +1,16 @@
 #include "lists.h"
 #include <stdio.h>
 
+/**
+ *print_dlistint - print the contents of a list
+ *@h: points to the first node of a dlist
+ *Return: number of elements
+ */
+
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count = 0;
+
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
