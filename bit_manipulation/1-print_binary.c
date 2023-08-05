e48e70db1504894bfee196a1a7b5f56ab0a87f74
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-08-05 03:32:51 +0000

    ./1-print_binary.c

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -3,20 +3,20 @@
 
 /**
  * print_binary - conver to binary
- * @n: number to convert 
+ * @n: number to convert
  *
  */
 void print_binary(unsigned long int n)
 {
-    if (n > 1)
-    {
-        print_binary(n >> 1);
-    }
-    putchar((n & 1) + '0');
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+	}
+	putchar((n & 1) + '0');
 
-    if (n > 1)
-    {
-        print_binary(n >> 1);
-    }
-    putchar((n & 1) + '0');
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+	}
+	putchar((n & 1) + '0');
 }
