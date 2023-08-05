15fa3bf1dd5365b805636df456de2ab607aff66c
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-08-04 23:31:27 -0400

    bit manipulation

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -7,32 +7,32 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    /*Declarations*/
-    dlistint_t *temp;
-    unsigned int count = 0;
+	/*Declarations*/
+	dlistint_t *temp;
+	unsigned int count = 0;
 
-    /*check if where pointed is in null*/
-    if (*head == NULL)
-        return (-1);
-    temp = *head;
-    if (index == 0)
-    {
-        *head = temp->next; /*Move head over*/
-        if (*head != NULL)
-            (*head)->prev = NULL; /*set to null if head is empty*/
-        free(temp);
-        return (1);
-    }
-    /*Move alon the list*/
-    while (temp != NULL && count < index)
-    {
-        /*Move and add to count*/
-        temp = temp->next;
-        count++;
-    }                              /*end while*/
-    temp->prev->next = temp->next; /*set the prev node to look next to*/
-    if (temp->next != NULL)
-        temp->next->prev = temp->prev; /*set next node prev to */
-    free(temp);
-    return (1);
+	/*check if where pointed is in null*/
+	if (*head == NULL)
+		return (-1);
+	temp = *head;
+	if (index == 0)
+	{
+		*head = temp->next; /*Move head over*/
+		if (*head != NULL)
+			(*head)->prev = NULL; /*set to null if head is empty*/
+		free(temp);
+		return (1);
+	}
+	/*Move alon the list*/
+	while (temp != NULL && count < index)
+	{
+		/*Move and add to count*/
+		temp = temp->next;
+		count++;
+	}                              /*end while*/
+	temp->prev->next = temp->next; /*set the prev node to look next to*/
+	if (temp->next != NULL)
+		temp->next->prev = temp->prev; /*set next node prev to */
+	free(temp);
+	return (1);
 } /*end function*/
