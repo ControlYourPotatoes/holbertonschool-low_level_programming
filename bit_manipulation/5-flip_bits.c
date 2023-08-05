6e7c145e0484ad529bfdad5b22afc3cb519b87ed
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-08-05 03:43:14 +0000

    ./5-flip_bits.c

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -7,15 +7,15 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-    unsigned int bits = 0;
-    unsigned long int xor ;
+	unsigned int bits = 0;
+	unsigned long int xor ;
 
-    xor = n ^ m;
+	xor = n ^ m;
 
-    while (xor)
-    {
-        bits += xor&1;
-        xor >>= 1;
-    }
-    return (bits);
+	while (xor)
+	{
+		bits += xor&1;
+		xor >>= 1;
+	}
+	return (bits);
 }
