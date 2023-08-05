2466a35a4e36d34179091d94e23efb183a1a6b91
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-08-05 03:33:14 +0000

    ./3-set_bit.c

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -8,12 +8,12 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-    if (index > 64)
-        return (-1);
-    if (!((*n >> index) & 1))
-    {
-        *n += 1 << index;
-        return (1);
-    }
-    return (-1);
+	if (index > 64)
+		return (-1);
+	if (!((*n >> index) & 1))
+	{
+		*n += 1 << index;
+		return (1);
+	}
+	return (-1);
 }
