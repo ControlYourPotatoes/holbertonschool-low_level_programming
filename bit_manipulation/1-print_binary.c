a52e624ad6eae0c456496861f141dd889df352da
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-08-05 03:37:00 +0000

    ./1-print_binary.c

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -12,11 +12,6 @@ void print_binary(unsigned long int n)
 	{
 		print_binary(n >> 1);
 	}
-	putchar((n & 1) + '0');
-
-	if (n > 1)
-	{
-		print_binary(n >> 1);
-	}
-	putchar((n & 1) + '0');
+	_putchar((n & 1) + '0');
 }
+
