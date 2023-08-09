3d537d369da98f8dc7b9f793264f910550182e08
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-08-09 17:22:08 -0400

    betty

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -11,40 +11,40 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int file;
-    char *buffer;
-    ssize_t bytesRead, bytesWritten;
+	int file;
+	char *buffer;
+	ssize_t bytesRead, bytesWritten;
 
-    if (filename == NULL)
-        return (0); /* Invalid filename */
+	if (filename == NULL)
+		return (0); /* Invalid filename */
 
-    file = open(filename, O_RDONLY);
-    if (file == -1)
-        return (0); /* Unable to open file */
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+		return (0); /* Unable to open file */
 
-    buffer = (char *)malloc(letters + 1);
-    if (buffer == NULL)
-    {
-        close(file);
-        return (0); /* Memory allocation failed */
-    }
+	buffer = (char *)malloc(letters + 1);
+	if (buffer == NULL)
+	{
+		close(file);
+		return (0); /* Memory allocation failed */
+	}
 
-    bytesRead = read(file, buffer, letters);
-    if (bytesRead <= 0)
-    {
-        free(buffer);
-        close(file);
-        return (0); /* Read error */
-    }
+	bytesRead = read(file, buffer, letters);
+	if (bytesRead <= 0)
+	{
+		free(buffer);
+		close(file);
+		return (0); /* Read error */
+	}
 
-    buffer[bytesRead] = '\0'; /* Null-terminate the buffer */
+	buffer[bytesRead] = '\0'; /* Null-terminate the buffer */
 
-    bytesWritten = write(STDOUT_FILENO, buffer, bytesRead);
-    free(buffer);
-    close(file);
+	bytesWritten = write(STDOUT_FILENO, buffer, bytesRead);
+	free(buffer);
+	close(file);
 
-    if (bytesWritten != bytesRead)
-        return (0); /* Write error */
+	if (bytesWritten != bytesRead)
+		return (0); /* Write error */
 
-    return (bytesWritten);
+	return (bytesWritten);
 }
