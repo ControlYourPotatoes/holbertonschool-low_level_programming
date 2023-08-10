3a1936e99e0f850d837c7c5a95e96507f920e43c
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-08-10 19:56:16 -0400

    betty main.h

--- file_io/main.h
+++ file_io/main.h
@@ -1,10 +1,13 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stddef.h>
+#ifndef main_h
+#define main_h
+#include <stdio.h>
+#include <string.h>
 #include <unistd.h>
-
-/* Read and print the contents of a text file */
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+/*Functionsignatures*/
 ssize_t read_textfile(const char *filename, size_t letters);
-
-#endif /* MAIN_H */
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+#endif
