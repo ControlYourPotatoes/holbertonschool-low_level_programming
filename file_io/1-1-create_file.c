25ae91d1d4a04e46f95e066d233fee4caf90b7d4
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-08-10 20:37:23 -0400

    indent

--- file_io/1-1-create_file.c
+++ file_io/1-1-create_file.c
@@ -13,31 +13,31 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    int file;
-    ssize_t bytesWritten;
+	int file;
+	ssize_t bytesWritten;
 
-    if (filename == NULL)
-    {
-        return (-1); /* Invalid filename */
-    }
+	if (filename == NULL)
+	{
+		return (-1); /* Invalid filename */
+	}
 
-    file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-    if (file == -1)
-    {
-        return (-1); /* Unable to create or open file */
-    }
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (file == -1)
+	{
+		return (-1); /* Unable to create or open file */
+	}
 
-    if (text_content != NULL)
-    {
-        bytesWritten = write(file, text_content, strlen(text_content));
-        if (bytesWritten == -1)
-        {
-            close(file);
-            return (-1); /* Write error */
-        }
-    }
+	if (text_content != NULL)
+	{
+		bytesWritten = write(file, text_content, strlen(text_content));
+		if (bytesWritten == -1)
+		{
+			close(file);
+			return (-1); /* Write error */
+		}
+	}
 
-    close(file);
+	close(file);
 
-    return (1);
+	return (1);
 }
